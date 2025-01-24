5c71200e48f7e22e160176de367f5b6144bd480a
oleg shchepotkin <music.wave.form7@gmail.com>
2025-01-24 17:25:28 +0300

    (env): change title

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ const { createApp, ref } = Vue
 
 createApp({
     setup() {
-      const message = ref('Hello, sick World!')
+      const message = ref('Hello, sick World! Have a nice day!')
       return {
         message
       }
