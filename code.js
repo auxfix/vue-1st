b500e76a032988bcb82b3a2c991f67ce239e4f9d
oleg shchepotkin <music.wave.form7@gmail.com>
2025-02-11 16:45:44 +0300

    (ui): change text

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ const { createApp, ref } = Vue
 
 createApp({
     setup() {
-      const message = ref('Hello!!')
+      const message = ref('Hello World!!')
       return {
         message
       }
